8ef188ef23ba64df96f197e2ad747e336082e946
Jon Peach <jonmakesitbetter@gmail.com>
2021-02-07 17:15:45 -0500

    Added test array and dom addition for future dynamic element generation

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-console.log("Yep, it worked");
+testArray = ["1", "2", "3"]
+
+for (var i = 0; i < testArray.length; i++){
+    const testCount = $("<h1>");
+
+    testCount.text(testArray[i]);
+    $("body").append(testCount);
+}
+
+
+document.getElementById("button").addEventListener("click", test);
+function test(){
+    console.log("ok, how about now?");
+}
+
+
+
 function openNav(){
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
